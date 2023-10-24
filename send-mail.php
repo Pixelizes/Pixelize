54e67b7a9095e97470a71bf9a221d6eee08a65b6
Pixelizes <rahatkhauk@gmail.com>
2023-10-25 00:06:52 +0500

    files

--- send-mail.php
+++ send-mail.php
@@ -15,7 +15,7 @@ $message=$_POST['message'];
     $body .= "Message: " . $message . "\n"; 
 
     //replace with your email
-    mail("atif.bhatti@solconsulting.com.pk","New email",$body); 
+    mail("test@email.com","New email",$body); 
 
   
 ?>
